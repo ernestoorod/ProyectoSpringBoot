4eac456dad0420bf4c9489a32f7151d38a4c6f9c
Ernesto Rodriguez Rodriguez <ernessttooo16@gmail.com>
2025-05-08 21:06:41 +0200

    commit 5

--- src/main/java/com/proyecto/service/articulo/ArticuloServiceImpl.java
+++ src/main/java/com/proyecto/service/articulo/ArticuloServiceImpl.java
@@ -1,8 +1,42 @@
 package com.proyecto.service.articulo;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import com.proyecto.model.articulo.ArticuloVO;
+
 @Service
-public class ArticuloServiceImpl implements ArticuloService {  
+public class ArticuloServiceImpl implements ArticuloService {
+
+    @Override
+    public ArticuloVO crear(ArticuloVO articuloVO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crear'");
+    }
+
+    @Override
+    public ArticuloVO actualizar(ArticuloVO articuloVO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+    @Override
+    public ArticuloVO obtenerPorId(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerPorId'");
+    }
+
+    @Override
+    public List<ArticuloVO> listarArticulos() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarArticulos'");
+    }  
 
 }
--- src/main/java/com/proyecto/service/usuario/UsuarioServiceImpl.java
+++ src/main/java/com/proyecto/service/usuario/UsuarioServiceImpl.java
@@ -1,8 +1,48 @@
 package com.proyecto.service.usuario;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.proyecto.model.usuario.UsuarioVO;
 
 @Service
 public class UsuarioServiceImpl implements UsuarioService {
+
+    @Override
+    public UsuarioVO crear(UsuarioVO usuarioVO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'crear'");
+    }
+
+    @Override
+    public UsuarioVO actualizar(UsuarioVO usuarioVO) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actualizar'");
+    }
+
+    @Override
+    public void eliminar(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+    }
+
+    @Override
+    public UsuarioVO obtenerPorId(Integer id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'obtenerPorId'");
+    }
+
+    @Override
+    public List<UsuarioVO> listarUsuarios() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'listarUsuarios'");
+    }
+
+    @Override
+    public UsuarioVO autenticar(String nombreUsuario, String contrasena) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'autenticar'");
+    }
 
 }
