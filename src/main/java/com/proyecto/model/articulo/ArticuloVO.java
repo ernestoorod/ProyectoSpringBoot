4e750d5a0e2837bdb1c50f52a9ebae0165880810
Ernesto Rodriguez Rodriguez <ernessttooo16@gmail.com>
2025-05-08 20:55:10 +0200

    commit 2

--- src/main/java/com/proyecto/model/articulo/ArticuloVO.java
+++ src/main/java/com/proyecto/model/articulo/ArticuloVO.java
@@ -1,5 +1,32 @@
 package com.proyecto.model.articulo;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "articulos")
 public class ArticuloVO {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id;
+
+    private String nombre;
+
+    private String descripcion;
+
+    private BigDecimal precio;
+
+    private int stock;
+
+    private boolean activo;
+
+    private Blob imagen;
+
 }
+
--- src/main/java/com/proyecto/model/usuario/UsuarioVO.java
+++ src/main/java/com/proyecto/model/usuario/UsuarioVO.java
@@ -1,5 +1,30 @@
 package com.proyecto.model.usuario;
 
+import jakarta.persistence.*;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuarios")
 public class UsuarioVO {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
+    private int id;
+
+    private String nombre;
+
+    private String apellidos;
+
+    private String nombreUsuario;
+
+    private String contrasena;
+
+    private String email;
+
+    private String telefono;
+
+    private String rol;
+
 }
